9ffbc31074bc2a7c1e1530519feaf5b258092619
xenobyte012 <xenobyte012@gmail.com>
2026-05-15 21:33:29 +0000

    removing react strict browser

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 )
